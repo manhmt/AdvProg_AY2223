755b76d3de8e39ba660b46123234723769b14225
Manhmai <mtmcomputerscience@gmail.com>
2023-05-09 22:20:57 +0700

    8/5.2

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -113,17 +113,11 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
-    for(int i = 0; i < word.length(); i++){
-         if(word[i] == ch){
-             secretWord[i] = ch;
-         }
-    }
-    /*for (int i = 0; i < word.length(); i++) {
+    for (int i = 0; i < word.length(); i++) {
         if (word[i] == ch) {
-            secretWord[2*i] = ch;
-            secretWord[2*i+1] = ' ';
+            secretWord[i] = ch;
         }
-    }*/
+    }
 }
 
 /***
